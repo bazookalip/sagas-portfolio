964d2fcc5f110dd9707c5ff766592e6399fa928c
Marcus Anderberg <marcusanderberg@gmail.com>
2019-03-09 14:33:50 -0600

    added images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+                <h1>Marcus Anderberg's</h1>
+                <p>Portfolio</p>
+            </div>
+        );
+    }
+}
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Header from '../Header/Header'
+
+
+class Home extends Component {
+
+
+    componentDidMount() {
+        this.getProjects();
+    }
 
 
 
+    getProjects = () => {
+        //make call to server using axios
+        console.log('going to get projects');
+        this.props.dispatch({ type: 'FETCH_PROJECTS' });
+    }
 
-class App extends Component {
+
+
+    adminList() {
+        return this.props.projects.map(project =>
+            <tr key={project.id}>
+                <td>{project.thumbnail}</td>
+                <td> {project.name} </td>
+                <td> {project.description} </td>
+                <td>{project.github}</td>
+               
+            </tr>
+        )
+    }
     // Renders the entire app on the DOM
     render() {
-        return (
+        let thumbnail;
+
+        if (this.props.projects.thumbnail === '') {
+            thumbnail = <img alt="Empty" src="images/empty.png" />
+        }
+        else if (this.props.projects.thumbnail === 'public/images/restaurant.png') {
+            thumbnail = <img alt="Restaurant Project" src="images/restaurants.png" />
+        }
+            return (
            <div>
-                 
+               <Header/>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>image</th>
+                            <th>Project</th>
+                            <th>Description</th>
+                            <th>Github</th>
+                        </tr>
+
+                        {this.adminList()}
+                    </tbody>
+                </table>
            </div>
         );
     }
 }
+const mapReduxToProps = (reduxState) => {
+    return reduxState;
+};
 
-export default App;
+export default connect(mapReduxToProps)(Home);
