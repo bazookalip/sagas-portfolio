b11e670ed90d30e05bbe03eef78a996ed0607bc7
Marcus Anderberg <marcusanderberg@gmail.com>
2019-03-10 21:37:26 -0500

    added link

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class ProjectItem extends Component {
     render() {
         return (
 
-           
-                <div className="container">
 
-                    {this.props.project.thumbnail !== "" && <img alt="" src={this.props.project.thumbnail} />}
+            <div className="container">
 
-                    <div>
-                        <h1> {this.props.project.name !== "" && this.props.project.name}</h1>
-                        <p>{this.props.project.github !== "" && this.props.project.github}</p>
-                        <p> {this.props.project.website !== "" && this.props.project.website}</p>
-                        <p>{this.props.project.tag_name !== "" && this.props.project.tag_name}</p>
-                      Date completed:  {this.props.project.date_completed !== "" && this.props.project.date_completed}
-                    </div>
-                    {this.props.project.description !== "" && this.props.project.description}
-           
+                {this.props.project.thumbnail !== "" && <img alt="" src={this.props.project.thumbnail} />}
 
+                <div>
+                    <h1> {this.props.project.name !== "" && this.props.project.name}</h1>
+                    <Link to="/"> {this.props.project.github !== "" && this.props.project.github}</Link> 
+                    <p> {this.props.project.website !== "" && this.props.project.website}</p>
+                    <p>{this.props.project.tag_name !== "" && this.props.project.tag_name}</p>
+                    Date completed:  {this.props.project.date_completed !== "" && this.props.project.date_completed}
+                </div>
+                {this.props.project.description !== "" && this.props.project.description}
 
 
+
+               
 
 
 
